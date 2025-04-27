050fbf3ab49447cfcbe8ae3a43cdbcb2aabd5f3d
rafipan <rafi.panyotova@gmail.com>
2025-04-27 14:15:44 +0100

    Initial commit of skeleton website

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector("#menu");
+    const frame = document.querySelector("#frame");
+    const frame_title = document.querySelector("#frame-title");
+
+    menu.addEventListener("click", (event) => {
+        let target = event.target.id;
+        console.log(target);
+        switch ( target ) {
+            case "about":
+                frame.src = "./about.html";
+                frame_title.textContent = "About me.txt";
+                break;
+            case "webdev":
+                frame.src = "./webdev.html";
+                frame_title.textContent = "Web dev.txt";
+                break;
+            case "links":
+                frame.src = "./links.html";
+                frame_title.textContent = "Links.txt";
+                break;
+        }
+    });
+});
